11c95ac8987ca5523d78bb355cf8124d0bd15ae0
Md. Abdulla-Al-Sun <sunkuet@hotmail.com>
2016-01-23 15:31:17 +0600

    Successful with taking grammer from file and Parse table generation

--- MyForm.h
+++ MyForm.h
@@ -34,6 +34,12 @@ namespace TextBox {
 	vector<int>tot_word;
 	hash_map<wstring, string> hash;
 	hash_map<wstring, string>::const_iterator it;
+	vector<string>splitgrammer[100][100];
+	vector<string>FIRST[100], FOLLOW[100];
+	vector<string>Nonterminal, terminal;
+	int totProduction[100];
+
+
 	bool notFound = false;
 	bool ischeckfinished;
 	int tot_line;
@@ -284,6 +290,7 @@ namespace TextBox {
 
 		public: void assignNpdaSymbols()
 		{
+
 					//Non-terminal
 
 					npdaSymbol["S"] = 1;
@@ -446,6 +453,273 @@ namespace TextBox {
 
 		public: void assignNumToSymbol()
 		{
+					StreamReader^ reader = File::OpenText("ppGrammers.txt");
+					String^ readString;
+					int idx = 1, idx_term = 1;
+					noOfSymbol["$"] = 0;
+
+					while ((readString = reader->ReadLine()) != nullptr)
+					{
+						string s = marshal_as<std::string>(readString);
+
+						string left = "";
+						int i = 0;
+						while (s[i] != '-')
+						{
+							left += s[i];
+							i++;
+						}
+
+						if (noOfSymbol.find(left) == noOfSymbol.end())
+						{
+							noOfSymbol[left] = idx++;
+							Nonterminal.push_back(left);
+						}
+
+						string temp = "";
+						int noofProduction = 1;
+						for (i = i + 1; s[i]; i++)
+						{
+							if (s[i] == ' ')
+							{
+								if (noOfSymbol.find(temp) == noOfSymbol.end())
+								{
+									if (temp[0] >= 'A'&&temp[0] <= 'Z')
+									{
+										noOfSymbol[temp] = idx++;
+										Nonterminal.push_back(temp);
+									}
+									else
+									{
+										noOfSymbol[temp] = idx_term++;
+										terminal.push_back(temp);
+									}
+								}
+
+								splitgrammer[noOfSymbol[left]][noofProduction].push_back(temp);
+								temp = "";
+							}
+							else if (s[i] == '|')
+							{
+								if (noOfSymbol.find(temp) == noOfSymbol.end())
+								{
+									if (temp[0] >= 'A'&&temp[0] <= 'Z')
+									{
+										noOfSymbol[temp] = idx++;
+										Nonterminal.push_back(temp);
+									}
+									else
+									{
+										noOfSymbol[temp] = idx_term++;
+										terminal.push_back(temp);
+									}
+								}
+								splitgrammer[noOfSymbol[left]][noofProduction].push_back(temp);
+								temp = "";
+								noofProduction++;
+							}
+							else
+								temp += s[i];
+						}
+						if (noOfSymbol.find(temp) == noOfSymbol.end())
+						{
+							if (temp[0] >= 'A'&&temp[0] <= 'Z')
+							{
+								noOfSymbol[temp] = idx++;
+								Nonterminal.push_back(temp);
+							}
+							else
+							{
+								noOfSymbol[temp] = idx_term++;
+								terminal.push_back(temp);
+							}
+						}
+
+						splitgrammer[noOfSymbol[left]][noofProduction].push_back(temp);
+						/*for(int i=1; i<=noofProduction; i++)
+						{
+						for(int j=0; j<parseTable[noOfSymbol[left]][i].size(); j++)
+						cout<<parseTable[noOfSymbol[left]][i][j]<<ends;
+						}
+						cout<<endl<<endl;
+						*/
+
+						totProduction[noOfSymbol[left]] = noofProduction;
+					}
+					/*
+					for(it=noOfSymbol.begin(); it!=noOfSymbol.end(); it++)
+					cout<<it->first<<" "<<it->second<<endl;
+
+					cout<<"Nonterminal "<<endl;
+
+					for(int i=0; i<Nonterminal.size(); i++)
+					cout<<Nonterminal[i]<<ends;
+					cout<<endl;
+
+					cout<<"terminal "<<endl;
+
+					for(int i=0; i<terminal.size(); i++)
+					cout<<terminal[i]<<ends;
+					cout<<endl;
+					*/
+
+					for (int i = 0; i<Nonterminal.size(); i++)
+					{
+						int main_idx = noOfSymbol[Nonterminal[i]];
+
+						queue<string>q;
+
+						q.push(Nonterminal[i]);
+
+						while (!q.empty())
+						{
+
+							string Non = q.front();
+							q.pop();
+							idx = noOfSymbol[Non];
+
+							//cout<<Nonterminal[i]<<" "<<totProduction[idx]<<endl;
+							for (int j = 1; j <= totProduction[idx]; j++)
+							{
+								//cout<<splitgrammer[idx][j][0]<<ends;
+								if (splitgrammer[idx][j][0][0] >= 'A'&&splitgrammer[idx][j][0][0] <= 'Z')
+									q.push(splitgrammer[idx][j][0]);
+								else
+									FIRST[main_idx].push_back(splitgrammer[idx][j][0]);
+							}
+						}
+						//cout<<endl;
+					}
+
+
+					for (int i = 0; i<Nonterminal.size(); i++)
+					{
+						idx = noOfSymbol[Nonterminal[i]];
+						if (Nonterminal[i] == "S")
+							FOLLOW[idx].push_back("$");
+						//cout<<Nonterminal[i]<<endl;
+						for (int k = 1; k <= totProduction[idx]; k++)
+						{
+							vector<string>v;
+							for (int j = 0; j<splitgrammer[idx][k].size(); j++)
+							{
+								//cout<<splitgrammer[idx][k][j]<<ends;
+								v.push_back(splitgrammer[idx][k][j]);
+
+							}
+							//cout<<endl<<endl;
+							if (v.size() == 1)
+							{
+								string tmp1 = v[0];
+								//cout<<"temp "<<tmp1<<" "<<Nonterminal[i]<<" "<<idx<<" "<<FOLLOW[idx].size()<<endl;
+								if ((tmp1[0] >= 'A'&&tmp1[0] <= 'Z'))
+								{
+									int idx2 = noOfSymbol[tmp1];
+									if (FOLLOW[idx2].size() == 0)
+									{
+										for (int x = 0; x<FOLLOW[idx].size(); x++)
+										{
+											FOLLOW[idx2].push_back(FOLLOW[idx][x]);
+											//cout<<FOLLOW[idx][x]<<ends;
+										}
+										FOLLOW[idx2].push_back("$");
+									}
+									//cout<<endl<<endl;
+								}
+							}
+							else
+							{
+								string tmp1, tmp2;
+								tmp1 = v[0];
+								tmp2 = v[1];
+								if ((tmp1[0] >= 'A'&&tmp1[0] <= 'Z') && (tmp2[0] >= 'A'&&tmp2[0] <= 'Z'))
+								{
+									int idx1 = noOfSymbol[tmp1];
+									int idx2 = noOfSymbol[tmp2];
+									if (FOLLOW[idx1].size() == 0)
+									for (int x = 0; x<FIRST[idx2].size(); x++)
+									{
+										FOLLOW[idx1].push_back(FIRST[idx2][x]);
+									}
+								}
+
+								if ((tmp1[0] >= 'a'&&tmp1[0] <= 'z') && (tmp2[0] >= 'A'&&tmp2[0] <= 'Z'))
+								{
+									int idx2 = noOfSymbol[tmp2];
+									if (FOLLOW[idx2].size() == 0)
+									for (int x = 0; x<FOLLOW[idx].size(); x++)
+									{
+										FOLLOW[idx2].push_back(FOLLOW[idx][x]);
+									}
+								}
+
+								tmp1 = v[v.size() - 1];
+								tmp2 = v[v.size() - 2];
+
+								if ((tmp1[0] >= 'A'&&tmp1[0] <= 'Z') && (tmp2[0] >= 'A'&&tmp2[0] <= 'Z'))
+								{
+									int idx1 = noOfSymbol[tmp1];
+									if (FOLLOW[idx1].size() == 0)
+										FOLLOW[idx1].push_back("$");
+								}
+							}
+						}
+					}
+
+
+
+					for (int i = 0; i<Nonterminal.size(); i++)
+					{
+						idx = noOfSymbol[Nonterminal[i]];
+
+						for (int j = 1; j <= totProduction[idx]; j++)
+						{
+
+							string str = splitgrammer[idx][j][0];
+
+							if (str[0] == '#')
+							{
+								for (int num = 0; num < FOLLOW[idx].size(); num++)
+								{
+									int idxTerminal = noOfSymbol[FOLLOW[idx][num]];
+
+									parseTable[idx][idxTerminal].push_back("#");
+
+									if (FOLLOW[idx][num] == "$")
+									{
+										parseTable[idx][idxTerminal].push_back("#");
+									}
+								}
+							}
+
+
+
+							else if (str[0] >= 'A' && str[0] <= 'Z')
+							{
+								int idxCurrent = noOfSymbol[str];
+
+								for (int num = 0; num < FIRST[idxCurrent].size(); num++)
+								{
+									int idxTerminal = noOfSymbol[FIRST[idxCurrent][num]];
+									for (int k = 0; k<splitgrammer[idx][j].size(); k++)
+										parseTable[idx][idxTerminal].push_back(splitgrammer[idx][j][k]);
+								}
+
+							}
+							else
+							{
+								int idxTerminal = noOfSymbol[str];
+
+								for (int k = 0; k<splitgrammer[idx][j].size(); k++)
+									parseTable[idx][idxTerminal].push_back(splitgrammer[idx][j][k]);
+							}
+						}
+
+					}
+
+					
+
+					/*
 			noOfSymbol["S"] = 1;
 			noOfSymbol["NP"] = 2;
 			noOfSymbol["NP1"] = 3;
@@ -467,13 +741,14 @@ namespace TextBox {
 			noOfSymbol["conj"] = 5;
 			noOfSymbol["verb"] = 6;
 			noOfSymbol["$"] = 7;
+			*/
 			return;
 		}
 
 		void genParseTable()
 		{
 			assignNumToSymbol();
-
+			/*
 			parseTable[1][1].push_back("NP");
 			parseTable[1][1].push_back("VP");
 			parseTable[1][2].push_back("NP");
@@ -549,12 +824,13 @@ namespace TextBox {
 			parseTable[11][7].push_back("#");
 
 			parseTable[12][6].push_back("verb");
-			parseTable[12][7].push_back("#");
+			parseTable[12][7].push_back("#");  
 
 			parseTable[13][4].push_back("adj");
 			parseTable[13][4].push_back("verb");
 
 			parseTable[13][6].push_back("verb");
+			*/
 			return;
 		}
 
@@ -612,7 +888,7 @@ namespace TextBox {
 						if (parseTable[nonIdx][terIdx][i] != "#")
 						{
 							st.push(parseTable[nonIdx][terIdx][i]);
-							//mainTextBox->Text += "  " + gcnew String(parseTable[nonIdx][terIdx][i].c_str());
+							mainTextBox->Text += "  " + gcnew String(parseTable[nonIdx][terIdx][i].c_str());
 							//mainTextBox->Text += "  " + i;
 						}
 					}
